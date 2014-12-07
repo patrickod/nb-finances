00b57fdf56bdf2b1ac88f326d5c1e7ecfdfe8d35
Patrick O'Doherty <p@trickod.com>
2014-12-07 14:03:39 -0800

    Added the noisetor_transactions VIEW

--- schema.sql
+++ schema.sql
@@ -49,6 +49,26 @@ CREATE TABLE paypal_transactions (
 ALTER TABLE public.paypal_transactions OWNER TO patrick;
 
 --
+-- Name: noisetor_transactions; Type: VIEW; Schema: public; Owner: patrick
+--
+
+CREATE VIEW noisetor_transactions AS
+ SELECT paypal_transactions.id,
+    paypal_transactions.type,
+    paypal_transactions.gross,
+    paypal_transactions.fee,
+    paypal_transactions.net,
+    paypal_transactions.from_email,
+    paypal_transactions.to_email,
+    paypal_transactions.title,
+    paypal_transactions.created_at
+   FROM paypal_transactions
+  WHERE ((paypal_transactions.title = 'Noisebridge Tor Node Project'::bpchar) OR (paypal_transactions.title ~~ 'Invoice%'::text));
+
+
+ALTER TABLE public.noisetor_transactions OWNER TO patrick;
+
+--
 -- Name: paypal_transactions_pkey; Type: CONSTRAINT; Schema: public; Owner: patrick; Tablespace: 
 --
 
@@ -57,12 +77,12 @@ ALTER TABLE ONLY paypal_transactions
 
 
 --
--- Name: public; Type: ACL; Schema: -; Owner: postgres
+-- Name: public; Type: ACL; Schema: -; Owner: patrick
 --
 
 REVOKE ALL ON SCHEMA public FROM PUBLIC;
-REVOKE ALL ON SCHEMA public FROM postgres;
-GRANT ALL ON SCHEMA public TO postgres;
+REVOKE ALL ON SCHEMA public FROM patrick;
+GRANT ALL ON SCHEMA public TO patrick;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
